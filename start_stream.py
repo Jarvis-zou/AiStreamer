3895c96392f72ed63babcf1c5eaefe3ed9b9a314
Jarvis-zou <429231345@qq.com>
2023-03-12 17:38:42 +0800

    增加语音输入和文本输入接口

--- start_stream.py
+++ start_stream.py
@@ -3,14 +3,16 @@ import os
 from utils import AiStreamer
 
 openai_key = os.environ.get('OPENAI_API_KEY')
+openai_key = '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'
 
 # set configuration
 parser = argparse.ArgumentParser()
-parser.add_argument("-model_name", type=str, help="Model type you want to choose")
+parser.add_argument("-model_name", type=str, default="gpt-3.5-turbo", help="Model type you want to choose")
 parser.add_argument("-streamer", type=str, help="Streamer name, should be same with instructions file name")
-parser.add_argument("--inputs_dir", type=str, default="./GPT_Inputs", help="Instructions dir path where instruction streamer.jsonl stores")
+parser.add_argument("--text_dir", type=str, default="./GPT_Inputs", help="Dir path where streamer.jsonl stores")
+parser.add_argument("--audio_dir", type=str, default="./Audio_Inputs", help="Dir path where streamer/streamer.mp3 stores")
 
 args = parser.parse_args()
 
 streamer = AiStreamer(openai_key, args)
-streamer.generate_text()
+result = streamer.get_inputs_from_typing(question='峰哥,你如何看待原神玩家?')
--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ import jsonlines
 
 class AiStreamer:
     def __init__(self, api_key, args):
-        self.api_key = api_key
+        openai.api_key = api_key
         self.args = args
 
     def read_jsonl(self):
@@ -15,17 +15,39 @@ class AiStreamer:
             contents: [list]
         """
         jsonl_name = self.args.streamer + '.jsonl'
-        jsonl_path = os.path.join(self.args.inputs_dir, jsonl_name)  # instruction文件的相对路径
+        jsonl_path = os.path.join(self.args.text_dir, jsonl_name)  # jsonl文件的相对路径
         with open(jsonl_path, 'r+', encoding='utf8') as f:
             contents = []
             for items in jsonlines.Reader(f):
                 contents.append(items)
         return contents
 
+    def write_jsonl(self, question):
+        """写入jsonl文件"""
+        contents = self.read_jsonl()
+        contents[-1]["content"] = question  # 写入新问题
+        jsonl_name = self.args.streamer + '.jsonl'
+        jsonl_path = os.path.join(self.args.text_dir, jsonl_name)  # jsonl文件的相对路径
+        with open(jsonl_path, 'w+', encoding='utf8') as f:
+            for content in contents:
+                f.write(str(content).replace('\'', '\"') + '\n')
+
+    def get_inputs_from_typing(self, question):
+        """替代音频输入，将问题写入jsonl文件中"""
+        self.write_jsonl(question)
+
+    def transcribe_audio(self):
+        """对指定音频进行语音识别"""
+        audio_file_path = os.path.join(os.path.join(self.args.audio_dir, self.args.streamer), 'audio_input.m4a')
+        audio_file = open(audio_file_path, 'rb')
+        transcript = openai.Audio.transcribe("whisper-1", audio_file)
+        sentences = transcript['text'].split(',')
+        sentences[0] = self.args.streamer  # 因为识别不准所以统一替换成指定的streamer名字
+        transcript = ','.join(sentences)
+        return transcript
+
     def generate_text(self):
         """从Inputs路径下的对应jsonl文件中读取用户的提问, 根据不同模型调用不同API并返回答案"""
-        openai.api_key = self.api_key
-
         # 区分3.5模型和其他老模型，因为接口调用方式不一样
         if self.args.model_name == 'gpt-3.5-turbo':
             massages = self.read_jsonl()
